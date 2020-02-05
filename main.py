ffad36f0850248233d7c944b4760af62c6afcae8
Lapua <c011731950@edu.teu.ac.jp>
2020-02-05 17:06:24 +0900

    何日目かを通知する機能を追加

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ INTERVAL = 3
 schedule = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, HOUR, MINUTE)
 tomorrow_schedule = schedule + datetime.timedelta(days=1)
 channel = None
+num_of_day = 1
+
+
+def print_next(message):
+    global num_of_day
+    await message.channel.send(schedule.strftime('%Y/%m/%d %H:%M') + ' - Day' + str(num_of_day))
+
 
 client = discord.Client()
 
@@ -18,7 +25,7 @@ client = discord.Client()
 async def loop():
     global schedule
     if channel is not None and datetime.datetime.now() >= schedule:
-        await channel.send('@everyone のんだ？')
+        await channel.send('@everyone のんだ？ - Day' + str(num_of_day))
         schedule += datetime.timedelta(minutes=INTERVAL)
 
 
@@ -31,50 +38,65 @@ async def on_ready():
 async def on_message(message):
     global schedule
     global tomorrow_schedule
+    global num_of_day
+    global channel
+
     if message.content == '/help':
         await message.channel.send('(のんだ | すてた) : その日の通知を消します\n'
                                    'のむよ (mins) : その日の通知を延長します\n'
                                    '/init : Botを起動してから最初だけうつコマンド\n'
                                    '/next : 次に通知する時間\n'
                                    '/stop (days) : 指定した日数だけ通知を止める\n'
-                                   '/reset : 通知を今日からする')
-    if message.content == 'のんだ' or message.content == 'すてた':
+                                   '/reset : 通知を今日からする\n'
+                                   '/day (day) : 次の通知日は何日目かをセットする')
+    elif message.content == 'のんだ' or message.content == 'すてた':
+        # 1時間以内かの判定
         margin = datetime.datetime.now()
         margin += datetime.timedelta(hours=1)
         if margin > schedule:
             schedule = tomorrow_schedule
             tomorrow_schedule += datetime.timedelta(days=1)
             await message.channel.send('Good! :)')
+
+            num_of_day += 1
+            if num_of_day > 28:
+                num_of_day = 1
         else:
             await message.channel.send('Error')
     elif message.content == '/init':
-        global channel
         channel = message.channel
+        await message.channel.send('何日目かをセットしてください. See help.')
     elif message.content == '/next':
-        await message.channel.send(schedule.strftime('%Y/%m/%d %H:%M'))
+        print_next(message)
     elif message.content.startswith('/stop '):
         days = message.content
         days = days[6:7]
         if days.isdecimal():
             schedule += datetime.timedelta(days=int(days))
             tomorrow_schedule += datetime.timedelta(days=int(days))
-            await message.channel.send(schedule.strftime('%Y/%m/%d %H:%M'))
+            print_next(message)
         else:
             await message.channel.send('Error')
     elif message.content == '/reset':
         schedule = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month,
                                      datetime.datetime.now().day, HOUR, MINUTE)
         tomorrow_schedule = schedule + datetime.timedelta(days=1)
-        await message.channel.send(schedule.strftime('%Y/%m/%d %H:%M'))
+        print_next(message)
     elif message.content.startswith('のむよ '):
         mins = message.content
         mins = mins[4:7]
         if mins.isdecimal():
             schedule += datetime.timedelta(minutes=int(mins))
-            await message.channel.send(schedule.strftime('%Y/%m/%d %H:%M'))
+            print_next(message)
         else:
             await message.channel.send('Error')
-
+    elif message.content == '/day ':
+        day = message.content
+        day = day[5:7]
+        if day.isdecimal():
+            num_of_day = int(day)
+        else:
+            await message.channel.send('Error')
 
 
 loop.start()
